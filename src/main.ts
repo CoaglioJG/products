70a00f3b5455b2815bec2fbdfdff4171861b7c70
joaocoaglio <joaocoaglio@brasilcard.net>
2022-01-14 15:21:52 -0300

    ajustando arquivo proto

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
-
-const microserviceOptions = {
-  transport: Transport.GRPC,
-  options: {
-    package:'products',
-    protoPath: join(__dirname, '../src/proto/products.proto'),
-  }
-}
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'products',
+        protoPath: join(__dirname, '../src/proto/products.proto'),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
--- src/presentation/controllers/products.controller.ts
+++ src/presentation/controllers/products.controller.ts
@@ -1,51 +1,59 @@
-import { Body, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { Products } from "src/domain/modules/entities/products";
-import { CloneProduct } from "src/domain/useCases/cloneProduct";
-import { DeleteProducts } from "src/domain/useCases/deleteProducts";
-import { GetProduct } from "src/domain/useCases/getProduct";
-import { ListProducts } from "src/domain/useCases/listProducts";
-import { SaveProducts } from "src/domain/useCases/saveProducts";
-import { UpdateProducts } from "src/domain/useCases/updateProducts";
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Products } from 'src/domain/modules/entities/products';
+import { CloneProduct } from 'src/domain/useCases/cloneProduct';
+import { DeleteProducts } from 'src/domain/useCases/deleteProducts';
+import { GetProduct } from 'src/domain/useCases/getProduct';
+import { ListProducts } from 'src/domain/useCases/listProducts';
+import { SaveProducts } from 'src/domain/useCases/saveProducts';
+import { UpdateProducts } from 'src/domain/useCases/updateProducts';
 
-export class ProductsController{
-    constructor(private readonly saveProducts: SaveProducts,
-        private readonly updateProducts: UpdateProducts, 
-        private readonly deleteProducts: DeleteProducts,
-        private readonly getProduct: GetProduct,
-        private readonly listProducts:ListProducts,
-        private readonly cloneProduct:CloneProduct,
-        ){}
+export class ProductsController {
+  constructor(
+    private readonly saveProducts: SaveProducts,
+    private readonly updateProducts: UpdateProducts,
+    private readonly deleteProducts: DeleteProducts,
+    private readonly getProduct: GetProduct,
+    private readonly listProducts: ListProducts,
+    private readonly cloneProduct: CloneProduct,
+  ) {}
 
-    @Get()
-    async listsProducts(): Promise<any> {
-        return await this.listProducts.call()
-    }
+  @Get()
+  @GrpcMethod('ProductService', 'find')
+  async listsProducts(): Promise<any> {
+    return await this.listProducts.call();
+  }
 
-    @Get(':name')
-    async Products(@Param('name') name:string): Promise<any> {
-        return await this.getProduct.call(name)
-    }
+  @Get(':name')
+  @GrpcMethod('ProductService', 'get')
+  async Products(@Param('name') name: string): Promise<any> {
+    return await this.getProduct.call(name);
+  }
 
-    @Post()
-    async addProducts(@Body() products:Products): Promise<any> {
-        return await this.saveProducts.call(products)
-    }
+  @Post()
+  @GrpcMethod('ProductService', 'create')
+  async addProducts(@Body() products: Products): Promise<any> {
+    return await this.saveProducts.call(products);
+  }
 
-    @Put(':name')
-    async putProducts(@Param('name') name:string ,@Body() elements:any): Promise<any>{
-        return await this.updateProducts.call(name, elements)
-    }
+  @Put(':name')
+  @GrpcMethod('ProductService', 'update')
+  async putProducts(
+    @Param('name') name: string,
+    @Body() elements: any,
+  ): Promise<any> {
+    return await this.updateProducts.call(name, elements);
+  }
 
-    @Delete('id')
-    async delPharmacy(@Param('id') id:number): Promise<any> {
-        return await this.deleteProducts.call(id)
-    }
+  @Delete('id')
+  @GrpcMethod('ProductService', 'del')
+  async delPharmacy(@Param('id') id: number): Promise<any> {
+    return await this.deleteProducts.call(id);
+  }
 
-    @Post(':id')
-    async cloneProducts(@Param('id') id:number): Promise<any>{
-        return await this.cloneProducts.call(id)
-    }
-
-   
-
+  @Post(':id')
+  @GrpcMethod('ProductService', 'clone')
+  async cloneProducts(@Param('id') id: number): Promise<any> {
+    return await this.cloneProduct.call(id);
+  }
 }
